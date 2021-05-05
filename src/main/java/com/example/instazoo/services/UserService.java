faf1dd79ee6d45c75907b54456ad2e7332a7ca46
noskov <2s8Bqx8NW>
2021-05-05 17:33:09 +0500

    UserService класс и собственные классы Ошибок

---
+++ src/main/java/com/example/instazoo/services/UserService.java
@@ -0,0 +1,45 @@
+package com.example.instazoo.services;
+
+import com.example.instazoo.entity.UserEntity;
+import com.example.instazoo.entity.enums.ERole;
+import com.example.instazoo.exceptions.UserExistException;
+import com.example.instazoo.payload.request.SignupRequest;
+import com.example.instazoo.repository.UserRepository;
+import org.omg.CORBA.UserException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+    public static final Logger LOG = LoggerFactory.getLogger(UserService.class);
+
+    private final UserRepository userRepository;
+    private final BCryptPasswordEncoder passwordEncoder;
+
+    @Autowired
+    public UserService(UserRepository userRepository, BCryptPasswordEncoder passwordEncoder) {
+        this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
+    }
+
+    public UserEntity createUser(SignupRequest userIn) {
+        UserEntity userEntity = new UserEntity();
+        userEntity.setEmail(userIn.getEmail());
+        userEntity.setUserName(userIn.getUsername());
+        userEntity.setLastName(userIn.getLastname());
+        userEntity.setFirstName(userIn.getFirstname());
+        userEntity.setPassword(passwordEncoder.encode(userIn.getPassword()));
+        userEntity.getRole().add(ERole.ROLE_USER);
+
+        try {
+            LOG.info("Saving User {}", userIn.getEmail());
+            return userRepository.save(userEntity);
+        } catch (Exception e) {
+            LOG.error("Error during registration {}", e.getMessage());
+            throw new UserExistException("The user " + userEntity.getUsername() + " already exist. Please check credentials");
+        }
+    }
+}

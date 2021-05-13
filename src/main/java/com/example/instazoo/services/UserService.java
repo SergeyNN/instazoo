07e891a475aa1bdc63926248583dff121fb5b013
noskov <2s8Bqx8NW>
2021-05-13 08:58:24 +0500

    User Service

--- src/main/java/com/example/instazoo/services/UserService.java
+++ src/main/java/com/example/instazoo/services/UserService.java
@@ -1,5 +1,6 @@
 package com.example.instazoo.services;
 
+import com.example.instazoo.dto.UserDTO;
 import com.example.instazoo.entity.UserEntity;
 import com.example.instazoo.entity.enums.ERole;
 import com.example.instazoo.exceptions.UserExistException;
@@ -9,8 +10,11 @@ import org.omg.CORBA.UserException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
+
+import java.security.Principal;
 
 @Service
 public class UserService {
@@ -41,5 +45,29 @@ public class UserService {
             LOG.error("Error during registration {}", e.getMessage());
             throw new UserExistException("The user " + userEntity.getUsername() + " already exist. Please check credentials");
         }
+    }
+
+    public UserEntity updateUser(UserDTO userDTO, Principal principal) {
+        UserEntity user = getUserByPrincipal(principal);
+        user.setFirstName(userDTO.getFirstname());
+        user.setLastName(userDTO.getLastname());
+        user.setBio(userDTO.getBio());
+
+        return userRepository.save(user);
+    }
+
+    public UserEntity getCurrentUser(Principal principal) {
+        return getUserByPrincipal(principal);
+    }
+
+    private UserEntity getUserByPrincipal(Principal principal) {
+        String username = principal.getName();
+        return userRepository.findUserEntityByUserName(username)
+                .orElseThrow(() -> new UsernameNotFoundException("Username not found with username " + username));
+
+    }
+
+    public UserEntity getUserById(Long id) {
+        return userRepository.findById(id).orElseThrow(() -> new UsernameNotFoundException("User not found"));
     }
 }

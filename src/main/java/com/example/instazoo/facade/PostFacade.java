07ec182454f70b844f0d5f632cd4765f88cc5807
noskov <2s8Bqx8NW>
2021-05-21 16:00:02 +0500

    User фасад Post фасад Comment фасад User контроллер Post контроллер Comment контроллер Image контроллер

---
+++ src/main/java/com/example/instazoo/facade/PostFacade.java
@@ -0,0 +1,23 @@
+package com.example.instazoo.facade;
+
+import com.example.instazoo.dto.PostDTO;
+import com.example.instazoo.entity.Post;
+import org.springframework.stereotype.Component;
+
+@Component
+public class PostFacade {
+
+    public PostDTO postToPostDTO(Post post) {
+        PostDTO postDTO = new PostDTO();
+        postDTO.setUsername(post.getUserEntity().getUsername());
+        postDTO.setId(post.getId());
+        postDTO.setCaption(post.getCaption());
+        postDTO.setLikes(post.getLikes());
+        postDTO.setUsersLiked(post.getLikedUsers());
+        postDTO.setLocation(post.getLocation());
+        postDTO.setTitle(post.getTitle());
+
+        return postDTO;
+    }
+
+}
